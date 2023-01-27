13090a7c6c77bd5475da9262e8870f53a0bff786
tareg <tareg.safi501@gmail.com>
2023-01-27 21:46:24 +0400

    first commit

---
+++ src/main/java/com/halfacode/springboot/kafka/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package com.halfacode.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "halfacode", groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("Message Recieved -> %s", message));
+
+    }
+}
+
---
+++ src/test/java/com/halfacode/springboot/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halfacode.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
